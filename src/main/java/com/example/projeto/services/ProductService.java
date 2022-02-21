50d71fb7b1dcf4f164164691a660431dcf4b313e
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-21 18:04:56 -0300

    Entidade Product

---
+++ src/main/java/com/example/projeto/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.example.projeto.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.projeto.entities.Product;
+import com.example.projeto.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public List<Product> findAll() {
+		return repository.findAll();
+
+	}
+
+	public Product FindById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+
+	}
+}
