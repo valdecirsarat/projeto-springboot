bce11d7aec3956de220f66fce833b05da8b50666
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-22 10:48:02 -0300

    Entidade OrderItem e associação muitos para muitos

--- src/main/java/com/example/projeto/config/TestConfig.java
+++ src/main/java/com/example/projeto/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.example.projeto.entities.Category;
 import com.example.projeto.entities.Order;
+import com.example.projeto.entities.OrderItem;
 import com.example.projeto.entities.Product;
 import com.example.projeto.entities.User;
 import com.example.projeto.entities.enums.OrderStatus;
 import com.example.projeto.repositories.CategoryRepository;
+import com.example.projeto.repositories.OrderItemRepository;
 import com.example.projeto.repositories.OrderRepository;
 import com.example.projeto.repositories.ProductRepository;
 import com.example.projeto.repositories.UserRepository;
@@ -32,6 +34,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 	
 
 	@Override
@@ -68,5 +73,14 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		
+		
 	}
 }
