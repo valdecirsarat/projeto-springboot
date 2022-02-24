92b2ca6fb98e09e76ddf9989d07cfd40ea336d66
Valdecir Sarat <valdecir.sarat@gmail.com>
2022-02-24 16:52:01 -0300

    tratamento exceção delete

--- src/main/java/com/example/projeto/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/projeto/resources/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.projeto.services.exceptions.DataBaseException;
 import com.example.projeto.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -22,5 +23,16 @@ public class ResourceExceptionHandler {
 		
 		return ResponseEntity.status(status).body(err);
 	}
+	
+	
+	@ExceptionHandler(DataBaseException.class)
+	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request){
+		String error = "Database Error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
 
+	
 }
--- src/main/java/com/example/projeto/services/UserService.java
+++ src/main/java/com/example/projeto/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.projeto.entities.User;
 import com.example.projeto.repositories.UserRepository;
+import com.example.projeto.services.exceptions.DataBaseException;
 import com.example.projeto.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -32,7 +35,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);			
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 
